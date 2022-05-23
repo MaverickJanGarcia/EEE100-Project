fffe8ce76bf9616832a0ef94619df84d3cd25acf
JessehKristineUreta <105429115+JessehKristineUreta@users.noreply.github.com>
2022-05-23 13:24:26 +0800

    Update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -1,3 +1,13 @@
 from tkinter import *
 import time
 
+class StopWatch(Frame):
+    def __init__(self, parent=None, **kw):
+        Frame.__init__(self, parent, kw)
+
+def main():
+    global root
+    root = Tk()
+    root.title("Stop Watch")
+
+  root.mainloop()
