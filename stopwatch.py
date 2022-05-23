7d3773a39d11f75495a42fb38bf140b376590f78
GwynneAcierda <105437606+GwynneAcierda@users.noreply.github.com>
2022-05-23 17:35:47 +0800

    I already created the required codes in #1.
    def Widgets(self) and def update(self) was already created and added in the code above.

--- stopwatch.py
+++ stopwatch.py
@@ -4,6 +4,20 @@ import time
 class StopWatch(Frame):
     def __init__(self, parent=None, **kw):
         Frame.__init__(self, parent, kw)
+        self.timedisplay = StringVar()
+        self.elapsedtime = 0.0
+        self.start = 0.0
+     
+    def Widgets(self):
+        # Stop Watch Timer
+        Label_StopWatchTime = Label(textvariable=self.timedisplay)
+        self.record_StopWatch(self.elapsedtime)
+        Label_StopWatchTime.place(x=180, y=185, height=25, width=150)
+    
+    def update(self):
+        self.elapsedtime = time.time() - self.start
+        self.record_StopWatch(self.elapsedtime)
+        self.timer = self.after(50, self.update)
 
 def main():
     global root
