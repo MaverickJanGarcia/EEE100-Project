c130b928677696eae7f16178eb28f4e7874ffbb3
RonaldPagal <105477312+RonaldPagal@users.noreply.github.com>
2022-05-26 06:46:21 +0800

    This fixes#2, resolved #9

--- stopwatch.py
+++ stopwatch.py
@@ -33,9 +33,9 @@ class StopWatch(Frame):
         
     def record_StopWatch(self, SWelapsedtime):
         SWhour = int(SWelapsedtime/3600) 
-        SWminute = int(SWelapsedtime/60)
-        SWsecond = int(SWelapsedtime - SWminute*60.0)
-        SWmillisecond = int((SWelapsedtime - SWminute*60.0 - SWsecond)*100)
+        SWminute = int(SWelapsedtime/60 - SWhour*60.0)
+        SWsecond = int(SWelapsedtime - SWhour*3600.0 - SWminute*60.0)
+        SWmillisecond = int((SWelapsedtime - SWhour*3600.0 - SWminute*60.0 - SWsecond)*100)
         self.timedisplay.set('%02d:%02d:%02d.%02d' % (SWhour, SWminute, SWsecond, SWmillisecond))
         
     def record_Lap(self, SWelapsedtime):
